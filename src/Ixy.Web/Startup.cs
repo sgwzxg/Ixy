0059793b8a09b12785a81c71f17e2da968898993
sgwzxg <geshiming@outlook.com>
2016-10-25 20:11:21 -0700

    improve areas

--- src/Ixy.Web/Startup.cs
+++ src/Ixy.Web/Startup.cs
@@ -84,12 +84,13 @@ namespace Ixy.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                   name: "areaRoute",
+                   template: "{area:exists}/{controller}/{action=Index}/{id?}");
 
                 routes.MapRoute(
-                    name: "areaRoute",
-                    template: "{area:exists}/{controller}/{action=Index}/{id?}");
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+               
             });
         }
     }
