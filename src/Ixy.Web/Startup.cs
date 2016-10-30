138a133ba6cd8b49865454cc82a508a7ba5bc5bb
sg-laptop <sgwzxg@hotmail.com>
2016-10-30 13:05:37 +0800

    add oauth

--- src/Ixy.Web/Startup.cs
+++ src/Ixy.Web/Startup.cs
@@ -1,12 +1,19 @@
 ﻿using Ixy.Core.Model.Identity;
 using Ixy.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Ixy.Web
 {
@@ -53,6 +60,77 @@ namespace Ixy.Web
             services.AddMvc();
         }
 
+        private OAuthOptions GithubOptions => new OAuthOptions
+        {
+
+            AuthenticationScheme = "Github",
+            DisplayName = "Github",
+            ClientId = Configuration["Github:ClientId"],
+            ClientSecret = Configuration["Github:ClientSecret"],
+            CallbackPath = new PathString("/signin-github"),
+            AuthorizationEndpoint = "https://github.com/login/oauth/authorize",
+            TokenEndpoint = "https://github.com/login/oauth/access_token",
+            UserInformationEndpoint = "https://api.github.com/user",
+            ClaimsIssuer = "OAuth2-Github",
+            SaveTokens = true,
+            Events = new OAuthEvents
+            {
+                OnCreatingTicket = async context => { await CreateGitHubAuthTicket(context); }
+            }
+        };
+
+        private static async Task CreateGitHubAuthTicket(OAuthCreatingTicketContext context)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await context.Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
+            response.EnsureSuccessStatusCode();
+            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            AddClaim(context, user);
+        }
+
+        private static void AddClaim(OAuthCreatingTicketContext context, JObject user)
+        {
+            var identifier = user.Value<string>("id");
+            if (!string.IsNullOrEmpty(identifier))
+            {
+                context.Identity.AddClaim(new System.Security.Claims.Claim(
+                    ClaimTypes.NameIdentifier, identifier,
+                    ClaimValueTypes.String, context.Options.ClaimsIssuer
+                    ));
+            }
+
+            var userName = user.Value<string>("login");
+            if (!string.IsNullOrEmpty(userName))
+            {
+                context.Identity.AddClaim(new Claim(
+                    ClaimsIdentity.DefaultNameClaimType, userName,
+                    ClaimValueTypes.String, context.Options.ClaimsIssuer
+                    ));
+            }
+
+            var name = user.Value<string>("name");
+            if (!string.IsNullOrEmpty(name))
+            {
+                context.Identity.AddClaim(new Claim(
+                    "urn:github:name", name,
+                    ClaimValueTypes.String, context.Options.ClaimsIssuer
+                    ));
+            }
+
+            var link = user.Value<string>("url");
+            if (!string.IsNullOrEmpty(link))
+            {
+                context.Identity.AddClaim(new Claim(
+                    "urn:github:url", link,
+                    ClaimValueTypes.String, context.Options.ClaimsIssuer
+                    ));
+            }
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
@@ -74,6 +152,8 @@ namespace Ixy.Web
 
             app.UseIdentity();
 
+            app.UseOAuthAuthentication(GithubOptions);
+
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
@@ -85,7 +165,7 @@ namespace Ixy.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-               
+
             });
         }
     }
