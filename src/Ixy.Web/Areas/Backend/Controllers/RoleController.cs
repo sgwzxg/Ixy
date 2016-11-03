60cdba8d92a6e374420ae1adaa4952e1e59589cb
sgwzxg <geshiming@outlook.com>
2016-11-03 03:19:31 -0700

    2

--- src/Ixy.Web/Areas/Backend/Controllers/RoleController.cs
+++ src/Ixy.Web/Areas/Backend/Controllers/RoleController.cs
@@ -3,8 +3,11 @@ using Ixy.Web.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Ixy.Web.Areas.Backend.Controllers
 {
@@ -26,5 +29,35 @@ namespace Ixy.Web.Areas.Backend.Controllers
         {
             return View(_roleManager.Roles.ToList());
         }
+
+        public async Task<IActionResult> Get(int startPage, int pageSize)
+        {
+            var role = await _roleManager.Roles.Skip((startPage - 1) * pageSize).Take(pageSize).ToListAsync();
+            var rowCount = role.Count();
+            return Json(
+                new
+                {
+                    rowCount = role.Count(),
+                    pageCount = Math.Ceiling(Convert.ToDecimal(rowCount) / pageSize),
+                    rows = role
+                }
+                );
+        }
+
+        public async Task<IActionResult> Get(string id)
+        {
+            var role = await _roleManager.Roles.Where(t => t.Id == id).ToListAsync();
+            var rowCount = role.Count();
+            return Json(
+                new
+                {
+                    rowCount = role.Count(),
+                    pageCount = 0,
+                    rows = role
+                }
+                );
+        }
+
+
     }
 }
---
+++ src/Ixy.Web/wwwroot/js/backend-role-index.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    $("#btnAdd").click(function () { add(1); });
+    $("#btnSave").click(function () { save(); });
+    $("#btnDelete").click(function () { deleteMulti(); });
+    loaddata();
+});
+
+function loadTables(startPage, pageSize) {
+    $("#tableBody").html("");
+    $("#checkAll").prop("checked", false);
+    $.ajax({
+        type: "GET",
+        url: "Backend/Role/Get?startPage=" + startPage + "&pageSize=" + pageSize,
+        success: function (data) {
+            $.each(data.rows, function (i, item) {
+                var tr = "<tr>";
+                tr += "<td align='center'><input type='checkbox' class='checkboxs' value='" + item.id + "'/></td>";
+                tr += "<td>" + item.name + "</td>";
+                tr += "<td><button class='btn btn-info btn-xs' href='javascript:;' onclick='edit(\"" + item.id + "\")'><i class='fa fa-edit'></i> Edit </button> <button class='btn btn-danger btn-xs' href='javascript:;' onclick='deleteSingle(\"" + item.id + "\")'><i class='fa fa-trash-o'></i> Delete </button> </td>"
+                tr += "</tr>";
+                $("#tableBody").append(tr);
+            })
+            var elment = $("#grid_paging_part");
+            if (data.rowCount > 0) {
+                var options = {
+                    bootstrapMajorVersion: 3,
+                    currentPage: startPage,
+                    numberOfPages: data.rowsCount,
+                    totalPages: data.pageCount,
+                    onPageChanged: function (event, oldPage, newPage) {
+                        loadTables(newPage, pageSize);
+                    }
+                }
+                elment.bootstrapPaginator(options);
+            }
+        }
+    })
+}
