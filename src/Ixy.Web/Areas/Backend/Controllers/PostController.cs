c07baeeb300bce1aa999a32e1d065dd43258a206
sg-laptop <sgwzxg@hotmail.com>
2016-11-07 08:12:24 +0800

    add authorize in PostController

--- src/Ixy.Web/Areas/Backend/Controllers/PostController.cs
+++ src/Ixy.Web/Areas/Backend/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using Ixy.Application.Service.Interface;
 using Ixy.Core.Model;
 using Ixy.Web.Controllers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace Ixy.Web.Areas.Backend.Controllers
 {
+    [Authorize]
     [Area("Backend")]
     public class PostController : IxyController
     {
